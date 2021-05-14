c04f508baa1afe01a3d9067a67c6fe79a737d9f5
mesode <akwe.mesode@gmail.com>
2021-05-14 11:08:43 +0100

    init

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import HttpExample from './http_example';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HttpExample />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// OR
+// const App = () => {
+//   return (
+//     <HttpExample />
+//   );
+// }
+// export default App
